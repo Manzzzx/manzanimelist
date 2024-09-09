181759d22dcd8c353dbac7f1989857cf22357bb9
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-09-09 11:09:32 +0700

    Sesi 4 - fix issues decode uri

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -3,13 +3,16 @@ import Header from "@/components/AnimeList/Header";
 
 const Page = async ({ params }) => {
   const { keyword } = params;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`);
+
+  const decodeKeyword = decodeURI(keyword);
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodeKeyword}`);
   const searchAnime = await response.json();
 
   return (
     <>
       <section>
-        <Header title={`Pencarian untuk ${keyword}...`} />
+        <Header title={`Pencarian untuk ${decodeKeyword}...`} />
         <AnimeList api={searchAnime} />
       </section>
     </>
