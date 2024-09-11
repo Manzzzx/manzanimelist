d67c9df59f684ec7be69bcc08ab9781a23bd7e82
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-09-11 22:19:55 +0700

    Sesi 5 - Reusable api call

---
+++ src/app/libs/api-libs.js
@@ -0,0 +1,8 @@
+// Untuk kebutuhan fetching api kita
+
+export const getAnimeResponse = async(resource, query) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`);
+    const anime = await response.json()
+
+    return anime
+}
--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,3 +1,4 @@
+import { getAnimeResponse } from "@/app/libs/api-libs";
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
 
@@ -5,10 +6,8 @@ const Page = async ({ params }) => {
   const { keyword } = params;
 
   const decodeKeyword = decodeURI(keyword);
-
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodeKeyword}`);
-  const searchAnime = await response.json();
-
+  const searchAnime = await getAnimeResponse ("anime", `q=${decodeKeyword}`);
+  
   return (
     <>
       <section>
