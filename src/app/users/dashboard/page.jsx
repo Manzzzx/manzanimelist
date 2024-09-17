95e123e272aaf92aabdfc6b19435e97bf340aef1
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-09-17 18:36:38 +0700

    Sesi 8 - Validasi routing dashboard user

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -6,8 +6,8 @@ const Page = async() => {
     return (
         <div className="text-color-primary" >
             <h3>DASHBOARD</h3>
-            <h5>Welcome, {user.name}</h5>
-            <Image src={user.image} alt="..." width={250} height={250} />
+            <h5>Welcome, {user?.name}</h5>
+            <Image src={user?.image} alt="..." width={250} height={250} />
         </div>
     )
 }
